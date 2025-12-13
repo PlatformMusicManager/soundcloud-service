f2ed7ef14ecbb9814ca7a8bfc509482b3d9cee56
Davidooff <david.ter.18@gmail.com>
2025-12-13 17:45:06 +0200

    added <<save>> query param to get playlist endpoint

--- src/routes/playlist.rs
+++ src/routes/playlist.rs
@@ -1,13 +1,16 @@
-use axum::extract::{Path, State};
+use aws_sdk_s3::config::auth::Params;
+use axum::extract::{Path, Query, State};
 use axum::Json;
 use domain::errors::app_error::AppError;
 use domain::models::db::soundcloud::{CreateReplacePlaylistInput, FullPlaylistResponse};
 use domain::models::music_api::playlist::ApiPlaylist;
 use crate::AppState;
+use crate::routes::SaveParams;
 
 pub async fn playlist(
     State(state): State<AppState>,
-    Path(id): Path<i64>
+    Path(id): Path<i64>,
+    Query(params): Query<SaveParams>,
 ) -> Result<Json<ApiPlaylist>, AppError> {
     match state.database.get_playlist_soundcloud(id).await? {
         Some(pl) => {
@@ -18,17 +21,19 @@ pub async fn playlist(
                 .get_playlist(&id.to_string())
                 .await?;
 
-            let playlist_create: CreateReplacePlaylistInput = playlist.clone().into();
+            if params.save {
+                let playlist_create: CreateReplacePlaylistInput = playlist.clone().into();
 
-            match state.database.replace_or_create_playlist_soundcloud(
-                &playlist_create.playlist,
-                &playlist_create.playlist_author,
-                &playlist_create.tracks,
-                &playlist_create.track_authors
-            ).await {
-                Ok(_) => {},
-                Err(e) => {eprintln!("ERROR: Can't add playlist to db, bks of{}", e);}
-            };
+                match state.database.replace_or_create_playlist_soundcloud(
+                    &playlist_create.playlist,
+                    &playlist_create.playlist_author,
+                    &playlist_create.tracks,
+                    &playlist_create.track_authors
+                ).await {
+                    Ok(_) => {},
+                    Err(e) => {eprintln!("ERROR: Can't add playlist to db, bks of{}", e);}
+                };
+            }
 
             Ok(Json(playlist.into()))
         }
